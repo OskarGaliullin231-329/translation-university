c208e7c13fe4d96055f655d0792ef36615363d43
OskarGaliullin231-329 <omg197323@gmail.com>
2026-04-18 13:16:41 +0300

    lexer-impl: moved showcases in separate directory

--- main.cpp
+++ main.cpp
@@ -4,11 +4,5 @@
 #include "include/preprocessor.h"
 
 int main() {
-    std::ifstream src_file;
-    std::string src_contents;
-    std::string line;
-    while (getline(src_file, src_contents)) {
-        src_contents
-    }
     return 0;
 }
