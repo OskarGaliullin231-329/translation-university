c11369af245bae1feba159e2cf55434a2323914d
OskarGaliullin231-329 <omg197323@gmail.com>
2026-04-26 01:27:20 +0300

    lexer-impl: added lexer.h

---
+++ include/lexer.h
@@ -0,0 +1,19 @@
+#ifndef LEXER_H_
+#define LEXER_H_
+
+#include <list>
+#include <utility>
+#include <string>
+
+enum class TokenType {
+    ID,
+    KEY,
+    DEL,
+    OP,
+    LIT,
+    ERR
+};
+
+void lexer_process(const std::string&, std::list<std::pair<std::string, TokenType>>&);
+
+#endif // LEXER_H_
